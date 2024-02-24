deb92f6efa99cabd5a73079b6104b6fecc986e88
Kirill Mahonin <murflexmur@yandex.ru>
2024-02-24 13:20:07 +0300

    ready

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
-// @todo: Темплейт карточки
+const addCard = ({ link = '', name = '' }) => {
+	const card = cardTemplate.cloneNode(true)
 
-// @todo: DOM узлы
+	image = card.querySelector('.card__image')
 
-// @todo: Функция создания карточки
+	image.src = link
+	image.alt = name
+	card.querySelector('.card__description').textContent = name
 
-// @todo: Функция удаления карточки
+	card
+		.querySelector('.card__delete-button')
+		.addEventListener('click', deleteCard)
 
-// @todo: Вывести карточки на страницу
+	placesList.append(card)
+}
+
+const deleteCard = e => {
+	const eventTarget = e.target.closest('.places__item')
+
+	if (!eventTarget) return
+
+	eventTarget.remove()
+}
+
+const cardTemplate = document
+	.querySelector('#card-template')
+	.content.querySelector('.places__item')
+
+const placesList = document.querySelector('.places__list')
+
+if (cardTemplate && placesList) {
+	initialCards.forEach(card => addCard(card))
+}
